22885f37531562c35011127a8fb15ded8496570a
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2023-03-16 21:27:12 -0700

    updated some css for navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,13 +23,14 @@ const api = axios.create({
 
 
 function App() {
-
     const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [showErr, setErr] = useState("");
     const [postResult, setPost] = useState<any[]>([]);
+    const dispearMenu = useRef("");
     const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);
 
     const handleLoginClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
@@ -77,14 +78,9 @@ function App() {
     };
 
 
-    const [isOpen, setIsOpen] = useState(false);
-
     const handleMenuToggle = () => {
         setIsOpen(!isOpen);
     };
-
-
-    const dispearMenu = useRef("");
 
     useEffect(() => {
       const handleClickOutside = (event: MouseEvent) => {
@@ -105,10 +101,9 @@ function App() {
         <div>
             {/* navbar */}
             <div >
-
                 <nav className='navbar flex flex-wrap items-center justify-between px-6 py-4 bg-gradient-to-r from-red-500 to-black-500'>
-                    <div ref={dispearMenu} className="flex items-center justify-between" >
-                        <div >
+                    <div ref={dispearMenu} className="flex items-center justify-between w-full" >
+                        <div className="flex items-center mr-auto" >
                             <Link to="/" className="flex items-center mr-10">
                                 <img className="h-12" src="./src/img/logo.png" alt="cookies logo"></img>
                             </Link>
@@ -117,13 +112,12 @@ function App() {
                         <button className="flex menu-btn menu-icon" onClick={handleMenuToggle}>
                             <i className={isOpen ? 'fas fa-times' : 'fas fa-bars'} />
                         </button>
-                        <ul className={`menu-list ${isOpen ? "active" : ""}`}>
+                        <ul className={`menu-list ${isOpen ? "active" : ""} flex-grow flex justify-end`}>
                             <li>
                                 <Link to="/" className="block mt-4 md:inline-block
                                      md:mt-0 mr-6">
                                     Home</Link>
-                            </li>
-                          
+                            </li>                          
                             <li>
                                 <Link to="/About" className="block mt-4 md:inline-block md:mt-0
                                       mr-6"> About
@@ -134,21 +128,20 @@ function App() {
                                      md:mt-0 mr-6">Contact</Link>
                             </li>
                             <li>
-                                <Link to="/Settings" className="block mt-4 md:inline-block
-                                     md:mt-0 mr-6">Settings</Link>
+                                <Link to="/Settings" className="block mt-4 md:inline-block md:mt-0 mr-6">Settings</Link>
                             </li>
                             <li className='avoidLi'>
                                 {!isAuthenticated && !isLoading ? (
-                                    <Link to="/"> <button onClick={handleLoginClick}>Log in</button> </Link>
+                                    <Link to="/"> <button className='py-3 px-5' onClick={handleLoginClick}>Log in</button> </Link>
                                 ) : (<Link to="/"> <button onClick={handleLogoutClick}
                                 className="py-2 px-4 "
                                 > Log Out</button></Link>
                                 )}
                             </li>
                         </ul>
-                        <div className="md:ml-4">
-                            <Link to="/Play" className="inline-block py-2 px-4 text-white rounded-full 
-                                            bg-red-700 hover:bg-red-600">Play</Link>
+                        <div className="mr-20">
+                            <Link to="/Play" className=" py-3 px-5
+                             text-white rounded-full bg-red-700 hover:bg-red-600">Play</Link>
                         </div>
                     </div>
                 </nav>
@@ -167,11 +160,9 @@ function App() {
                 {isAuthenticated ? (
                     <div>
                         <p className="readFont">Logged in as: {user?.nickname}</p>
-
                         <button onClick={handleLogoutClick}>Logout</button>
                     </div>
                 ) : ""}
-
             </div>
             <nav className="navbar">
                 <div className="navbar-container">
