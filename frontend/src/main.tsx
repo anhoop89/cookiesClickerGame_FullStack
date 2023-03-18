754c6ba720df76de99b6519f51c1a37ff2793e99
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2023-03-18 04:19:22 -0700

    the docker of the backend is still failed - attempted to send a GET /users/ FAILED

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./CSS/index.css";
-import dotenv from "dotenv";
-const env = dotenv.config();
 
 import { Auth0Provider } from "@auth0/auth0-react";
 
@@ -12,13 +10,15 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Auth0Provider
-        domain="env.VITE_CLICKERS_APP_AUTH0_DOMAIN"
-        clientId="env.VITE_CLICKERS_APP_AUTH0_CLIENT_ID"
+        domain="anhoop89.us.auth0.com"
+        clientId="3p2fQA9DF5ml0KwVJGmpJcPdYKG2MdKp"
         authorizationParams={{ redirect_uri: window.location.origin }}
       >
+        
         <App />
         {/* <Navbar /> */}
       </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
+
