0379af292566e2b05a47f4712c2b41e73f0f6891
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2023-03-19 22:26:38 -0700

    update CSS to fix the nignx issue

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -59,7 +59,8 @@ function About() {
 
         <div className="flex flex-col md:flex-row">
           <div className="md:w-1/2 flex justify-center">
-            <img className="rounded-full h-60 w-60 object-cover shadow-2xl" src="./src/img/nicholas.png" alt="Nicholas Nguyen" />
+            <div className="logoBanner" alt="aboutTwo"  alt="Nicholas Nguyen" ></div>
+            
           </div>
           <div className="md:w-1/2 px-4 py-2 contactFont descriptorText ">
             <h2 className="text-3xl mb-2"> <strong> Nicholas Nguyen (he/him) </strong></h2>
